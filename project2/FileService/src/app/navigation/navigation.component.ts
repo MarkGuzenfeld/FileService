588b529dd2500c991c92f47ff10825c698c8c3cb
Mark_Guzenfeld <m.dfr@yandex.ru>
2022-12-25 09:59:05 +0200

    Subject работает в файле navigation.component но не получается передать на него возвращание "домой"

--- project2/FileService/src/app/navigation/navigation.component.ts
+++ project2/FileService/src/app/navigation/navigation.component.ts
@@ -11,17 +11,17 @@ import { DataTableComponent } from '../data-table/data-table.component';
   styleUrls: ['./navigation.component.css']
 })
 export class NavigationComponent {
-  sub: Subscription | undefined
-  stream$: Subject<number>= new Subject<number>();
-  constructor() {
-    this.sub = this.stream$.subscribe( value =>{
-      console.log('asd', value); 
+  stream$: Subject<string> = new Subject<string>();
+  constructor (){
+    this.stream$.subscribe(val =>{
+      console.log(12);
+      return ;
+      
     })
   }
- next (){
-  this.stream$.next;
- }
-
+  next(){
+    this.stream$.next('')
+  }
 //     constructor (private DataTableComponent: DataTableComponent){
 //   this.DataTableComponent.homeGoBAck().subscribe(()=>{
 //     return this.retutnToHome.asObservable();
